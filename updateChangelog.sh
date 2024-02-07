e268562700dd5d237f7faf7ca08de73da1a1b301
= <xysiek99@gmail.com>
2024-02-07 19:49:27 +0100

    Added colon

--- updateChangelog.sh
+++ updateChangelog.sh
@@ -3,4 +3,4 @@
 CURRENT_TIME=$(date "+%D %T")
 CHANGELOG_FILE="changelog.txt"
 
-echo "Pipeline triggered $CURRENT_TIME" >> $CHANGELOG_FILE
+echo "Pipeline triggered: $CURRENT_TIME" >> $CHANGELOG_FILE
